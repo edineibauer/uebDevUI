88648b3f3e64fe7eea96c8f4f1c48442f5fbfd22
Edinei <galera.org@gmail.com>
2018-12-12 20:19:51 -0200

    integrações instead constantes

--- public/view/devPages/integracoes.php
+++ public/view/devPages/integracoes.php
@@ -12,9 +12,9 @@ if(file_exists(PATH_HOME . "_config/config.json"))
     $dados = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
 
 foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR) as $item) {
-    if(file_exists(PATH_HOME . VENDOR . $item . "/public/constante")){
-        foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR . $item . "/public/constante") as $c) {
-            $file = json_decode(file_get_contents(PATH_HOME . VENDOR . $item . "/public/constante/{$c}"), true);
+    if(file_exists(PATH_HOME . VENDOR . $item . "/public/integracoes")){
+        foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR . $item . "/public/integracoes") as $c) {
+            $file = json_decode(file_get_contents(PATH_HOME . VENDOR . $item . "/public/integracoes/{$c}"), true);
             foreach ($file['constantes'] as $nome => $column)
                 $file['constantes'][$nome]['value'] = $dados[$file['constantes'][$nome]['column']] ?? "";
 
