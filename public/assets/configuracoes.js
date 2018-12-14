aacb9d47c0686cb7d87c2b70832f45e03b97e2cd
Edinei <galera.org@gmail.com>
2018-12-14 18:06:56 -0200

    remove cache

--- public/assets/configuracoes.js
+++ public/assets/configuracoes.js
@@ -2,9 +2,24 @@ function updateConfig() {
     toast('Atualizando...', 8000, 'toast-success');
     setTimeout(function () {
         post("dev-ui", "cache/update", function () {
+
+            //delete service workers
+            navigator.serviceWorker.getRegistrations().then(function (registrations) {
+                for (let registration of registrations) {
+                    registration.unregister()
+                }
+            });
+
+            //delete caches
+            caches.keys().then(cacheNames => {
+                return Promise.all(cacheNames.map(cacheName => {
+                    return caches.delete(cacheName)
+                }))
+            });
+
             setTimeout(function () {
                 location.reload();
-            }, 700)
+            }, 700);
         })
     }, 500);
 }
