52b87c3707627208d7c1a05debce7a01b7dfe6ef
Edinei <galera.org@gmail.com>
2019-01-23 16:57:05 -0200

    bloqueia ação ao aplicar tema

--- public/assets/themeMake.js
+++ public/assets/themeMake.js
@@ -249,29 +249,38 @@ function createTheme() {
     displayDemo(theme1);
 }
 
+var applyTheme = 0;
 function restoreTheme() {
-    toast("Restaurando...", 6000, "toast-success");
-    post('dev-ui', 'tema/restoreTheme', function (d) {
-        if (d === "no") {
-            toast("tema anterior não encontrado", 3000, "toast-warning");
-        } else {
-            setUpdateVersion();
-            clearCache().then(() => {
-                window.location.reload()
-            });
-        }
-    });
+    if(applyTheme === 0) {
+        applyTheme = 1;
+        toast("Restaurando...", 6000, "toast-success");
+        post('dev-ui', 'tema/restoreTheme', function (d) {
+            if (d === "no") {
+                toast("tema anterior não encontrado", 3000, "toast-warning");
+            } else {
+                toast("Tema Alterado", 1000, "toast-success");
+                setUpdateVersion();
+                clearCache().then(() => {
+                    window.location.reload()
+                });
+            }
+        });
+    }
 }
 
 function saveTheme() {
     if(localStorage.txt) {
-        toast("Aplicando...", 6000, "toast-success");
-        post('dev-ui', 'tema/saveTheme', {txt: localStorage.txt}, function () {
-            setUpdateVersion();
-            clearCache().then(() => {
-                window.location.reload()
+        if(applyTheme === 0) {
+            applyTheme = 1;
+            toast("Aplicando...", 6000, "toast-success");
+            post('dev-ui', 'tema/saveTheme', {txt: localStorage.txt}, function () {
+                toast("Tema Alterado", 1000, "toast-success");
+                setUpdateVersion();
+                clearCache().then(() => {
+                    window.location.reload()
+                });
             });
-        });
+        }
     } else {
         toast("selecione um tema antes", 3000, "toast-warning");
     }
