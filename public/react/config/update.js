be0d9f785d2a06e8f169b5c858c7814eca29e66b
Edinei <galera.org@gmail.com>
2019-01-23 11:35:10 -0200

    remove react config update

--- public/react/config/update.js
+++
@@ -1,6 +0,0 @@
-toast('Atualizando...', 8000, 'toast-success');
-post("dev-ui", "cache/update", function () {
-    post("config", "updateConfiguracoes", function () {
-        updateVersion();
-    });
-});
