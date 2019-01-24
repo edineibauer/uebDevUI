d82c3ed61dd8f9b34bb04231a1e8852d7b3f0e69
Edinei <galera.org@gmail.com>
2019-01-24 17:32:10 -0200

    autor

--- public/assets/autores.js
+++ public/assets/autores.js
@@ -1,1 +1,21 @@
-$("#autor-list-dev").grid("teste", [], {autor: true, update: false, delete: false, status: false});
+$(function () {
+    let dicionarios = dbLocal.exeRead("__dicionario", 1);
+    let tpl = dbLocal.exeRead("__template", 1);
+    Promise.all([dicionarios, tpl]).then(r => {
+        dicionarios = r[0];
+        tpl = r[1];
+
+        let entitys = [];
+        let classe = 'color-gray-light';
+        $.each(dicionarios, function (entity, meta) {
+            classe = (classe === 'color-white' ? 'color-gray-light' : 'color-white');
+            entitys.push({class: classe, entity: entity});
+        })
+
+        $("#autor-list-dev").html(Mustache.render(tpl['autor-list-entity'], {entidades: entitys}));
+    });
+
+    $("#autor-list-dev").off("click", ".btn-edit-autor").on("click", ".btn-edit-autor", function () {
+        $("#autor-list-dev").html("").grid($(this).attr("rel"), [], {autor: !0, create: !1, update: !1, delete: !1, status: !1})
+    });
+});
