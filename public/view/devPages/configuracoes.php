c08f181fe7a8b346ad038bd922e4932ea1ab2c71
Edinei J. Bauer <galera.org@gmail.com>
2019-03-30 12:07:01 -0300

    config autosync and limit offline

--- public/view/devPages/configuracoes.php
+++ public/view/devPages/configuracoes.php
@@ -3,7 +3,6 @@
 $read = new \Conn\Read();
 $read->exeRead(PRE . "config", "WHERE id=1");
 if (!$read->getResult()) {
-
     $criarData = [
         "nome_do_site" => defined('SITENAME') && !empty(SITENAME) ? SITENAME : "",
         "subtitulo" => defined('SITESUB') && !empty(SITESUB) ? SITESUB : "",
@@ -18,6 +17,38 @@ if (!$read->getResult()) {
     $d->setData($criarData);
     $d->save();
 }
+
+$conf = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
+
 ?>
 <div class="col padding-medium font-medium" id="config-form-dev"></div>
-<script>$("#config-form-dev").form("config",1)</script>
+
+<div class="col padding-12">
+    <div class="left container">
+        <label class="col card padding-medium padding-16">
+            <input type="checkbox" class="left margin-left" id="autosync"
+                   value="<?= $conf['autosync'] ?>" <?= ($conf['autosync'] ? "checked='checked'" : "") ?>/>
+            <div class="font-medium left padding-8 padding-right pointer">Sincronização Automática</div>
+        </label>
+    </div>
+    <div class="left container">
+        <label class="col card padding-medium" style="padding-bottom: 0!important;width: 160px;">
+            <span class="col align-left">Limite de Registros Offline</span>
+            <input type="number" step="50" class="left" id="limitoffline" value="<?= $conf['limitoffline'] ?>"/>
+            <span class="input-bar"></span>
+        </label>
+    </div>
+</div>
+
+
+<script>
+    $("#config-form-dev").form("config", 1, ["nome_do_site", "subtitulo", "descricao"]);
+
+    function postOptions() {
+        post("config", "updateOptions", {'autosync': $("#autosync").is(":checked"), 'limitoffline': $("#limitoffline").val()});
+    }
+
+    $("#autosync, #limitoffline").off("change keyup").on("change keyup", function() {
+        postOptions();
+    });
+</script>
