93bdb17f4a6301060c9a35123f5fd107a2b0efd2
Edinei J. Bauer <galera.org@gmail.com>
2019-10-27 20:49:32 -0300

    image localStorage fixed

--- public/assets/UIDev.js
+++ public/assets/UIDev.js
@@ -14,7 +14,7 @@ function devSidebarInfo() {
     if (localStorage.imagem === "" || localStorage.imagem === "null") {
         document.querySelector("#dev-sidebar-imagem").innerHTML = "<i class='material-icons font-jumbo'>people</i>"
     } else {
-        document.querySelector("#dev-sidebar-imagem").innerHTML = "<img src='" + decodeURIComponent(JSON.parse(localStorage.imagem)[0]['urls'][100]) + "' height='60' width='60'>"
+        document.querySelector("#dev-sidebar-imagem").innerHTML = "<img src='" + decodeURIComponent(JSON.parse(localStorage.imagem)['urls'][100]) + "' height='60' width='60'>"
     }
     document.querySelector("#dev-sidebar-nome").innerHTML = getCookie("nome");
 
