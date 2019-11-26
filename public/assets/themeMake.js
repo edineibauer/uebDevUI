4742695064cb674b42b21fd8148b8de07dd30b6b
Edinei J. Bauer <galera.org@gmail.com>
2019-11-26 08:39:41 -0300

    theme maker more easy between intense

--- public/assets/themeMake.js
+++ public/assets/themeMake.js
@@ -41,7 +41,7 @@ function createTheme() {
     sat = x.s;
     light = x.l;
 
-    x.l= light + ((1.0-light)/5) * 4.7;
+    x.l= light + ((1.0-light)/5) * 2.5;
     hex = w3color("hsl(" + x.h + "," + sat + "," + x.l + ")").toHexString();
     z = document.getElementById("t1l5");
     z.style.backgroundColor = hex;
@@ -52,7 +52,7 @@ function createTheme() {
     z.style.color = col;
     theme1.tl5 = col;
 
-    x.l= light + ((1.0-light)/5) * 4;
+    x.l= light + ((1.0-light)/5) * 2;
     hex = w3color("hsl(" + x.h + "," + sat + "," + x.l + ")").toHexString();
     z = document.getElementById("t1l4");
     z.style.backgroundColor = hex;
@@ -63,7 +63,7 @@ function createTheme() {
     z.style.color = col;
     theme1.tl4 = col;
 
-    x.l= light + ((1.0-light)/5) * 3;
+    x.l= light + ((1.0-light)/5) * 1.3;
     hex = w3color("hsl(" + x.h + "," + sat + "," + x.l + ")").toHexString();
     z = document.getElementById("t1l3");
     z.style.backgroundColor = hex;
@@ -74,7 +74,7 @@ function createTheme() {
     z.style.color = col;
     theme1.tl3 = col;
 
-    x.l= light + ((1.0-light)/5) * 2;
+    x.l= light + ((1.0-light)/5) * 0.8;
     hex = w3color("hsl(" + x.h + "," + sat + "," + x.l + ")").toHexString();
     z = document.getElementById("t1l2");
     z.style.backgroundColor = hex;
@@ -85,7 +85,7 @@ function createTheme() {
     z.style.color = col;
     theme1.tl2 = col;
 
-    x.l= light + ((1.0-light)/5) * 1;
+    x.l= light + ((1.0-light)/5) * 0.3;
     hex = w3color("hsl(" + x.h + "," + sat + "," + x.l + ")").toHexString();
     z = document.getElementById("t1l1");
     z.style.backgroundColor = hex;
@@ -107,7 +107,7 @@ function createTheme() {
     z.style.color = col;
     theme1.td0 = col;
 
-    x.l= light - ((light)/5) * 0.5;
+    x.l= light - ((light)/5) * 0.3;
     hex = w3color("hsl(" + x.h + "," + x.s + "," + x.l + ")").toHexString();
     z = document.getElementById("t1d1");
     z.style.backgroundColor = hex;
@@ -118,7 +118,7 @@ function createTheme() {
     z.style.color = col;
     theme1.td1 = col;
 
-    x.l= light - ((light)/5) * 1;
+    x.l= light - ((light)/5) * 0.7;
     hex = w3color("hsl(" + x.h + "," + x.s + "," + x.l + ")").toHexString();
     z = document.getElementById("t1d2");
     z.style.backgroundColor = hex;
@@ -129,7 +129,7 @@ function createTheme() {
     z.style.color = col;
     theme1.td2 = col;
 
-    x.l= light - ((light)/5) * 1.5;
+    x.l= light - ((light)/5) * 1.3;
     hex = w3color("hsl(" + x.h + "," + x.s + "," + x.l + ")").toHexString();
     z = document.getElementById("t1d3");
     z.style.backgroundColor = hex;
@@ -198,7 +198,7 @@ function createTheme() {
         /* TEMA ADICONAL PARA TROCA DE ELEMENTOS PADRÕES */
         /*===================================================*/
 
-        txt += ".switch input:checked + .slider {background-color:" + theme1.l2 + "!important}<br>";
+        txt += ".switch input:checked + .slider {background-color:" + theme1.l1 + "!important}<br>";
         txt += ".switch input:checked + .slider:after {background-color:" + theme1.d0 + "!important}<br>";
 
         /* INPUT BAR STYLE */
