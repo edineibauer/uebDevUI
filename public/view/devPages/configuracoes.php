19afa5b5db0dbc1efca013fdf8ff9e15eade11b7
Edinei J. Bauer <galera.org@gmail.com>
2020-01-07 17:17:19 -0300

    remove configs

--- public/view/devPages/configuracoes.php
+++ public/view/devPages/configuracoes.php
@@ -1,36 +1,8 @@
 <?php
-//Garante criação dos dados de configuração caso não exista
-$read = new \Conn\Read();
-$read->exeRead(PRE . "config", "WHERE id=1");
-if (!$read->getResult()) {
-    $criarData = [
-        "nome_do_site" => defined('SITENAME') && !empty(SITENAME) ? SITENAME : "",
-        "subtitulo" => defined('SITESUB') && !empty(SITESUB) ? SITESUB : "",
-        "descricao" => defined('SITEDESC') && !empty(SITEDESC) ? SITEDESC : "",
-        "https" => defined('PROTOCOL') && !empty(PROTOCOL) && PROTOCOL === "https://" ? 1 : 0,
-        "www" => defined('WWW') && !empty(WWW) && WWW === "www" ? 1 : 0,
-        "logo" => defined('LOGO') && !empty(LOGO) ? '[{"url": "' . LOGO . '", "name": "logo", "nome": "logo", "size": 66582, "type": "png", "image": "' . LOGO . '", "fileType": "image/png", "sizeName": "66KB"}]' : null,
-        "favicon" => defined('FAVICON') && !empty(FAVICON) ? '[{"url": "' . FAVICON . '", "name": "favicon", "nome": "favicon", "size": 66582, "type": "png", "image": "' . FAVICON . '", "fileType": "image/png", "sizeName": "66KB"}]' : null,
-    ];
-
-    $d = new \Entity\Dicionario("config");
-    $d->setData($criarData);
-    $d->save();
-}
-
 $conf = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
 
 ?>
-<div class="col padding-medium font-medium" id="config-form-dev"></div>
-
 <div class="col padding-12">
-    <!--<div class="left container">
-        <label class="col card padding-medium padding-16">
-            <input type="checkbox" class="left margin-left" id="autosync"
-                   value="<?/*= $conf['autosync'] */?>" <?/*= ($conf['autosync'] ? "checked='checked'" : "") */?>/>
-            <div class="font-medium left padding-8 padding-right pointer">Sincronização Automática</div>
-        </label>
-    </div>-->
     <div class="left container">
         <label class="col card padding-medium padding-16">
             <input type="checkbox" class="left margin-left" id="homepage"
@@ -61,10 +33,7 @@ $conf = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
     </div>
 </div>
 
-
 <script>
-    $("#config-form-dev").form("config", 1, ["nome_do_site", "subtitulo", "descricao"]);
-
     function postOptions() {
         post("config", "updateOptions", {'autosync': $("#autosync").is(":checked"), 'dev': $("#dev").is(":checked"), 'serviceworker': $("#serviceworker").is(":checked"), 'homepage': $("#homepage").is(":checked"), 'limitoffline': $("#limitoffline").val()});
     }
