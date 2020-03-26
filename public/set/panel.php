3813e27c61031d202e1a729790b73be0a940f753
Edinei J. Bauer <galera.org@gmail.com>
2020-03-26 15:56:24 -0300

    fix panel

--- public/set/panel.php
+++ public/set/panel.php
@@ -5,9 +5,9 @@ $data = ["response" => 1, "data" => "<div style='float: left; width: 100%; paddi
 $menus = [];
 
 if ($_SESSION['userlogin']['id'] === "1")
-    $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/devPages/inc/menuMaster.json"), !0);
+    $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/inc/menuMaster.json"), !0);
 elseif ($_COOKIE['setor'] === "admin")
-    $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/devPages/inc/menu.json"), !0);
+    $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/inc/menu.json"), !0);
 
 foreach ($menus as $menu)
     $data['data'] .= $tpl->getShow("menu-card", $menu);
