3369603df133bc582b7824597bc5e0fa0b85c943
Edinei J. Bauer <galera.org@gmail.com>
2020-04-02 10:01:52 -0300

    include public folder integracoes

--- public/view/UIDevIntegracoes.php
+++ public/view/UIDevIntegracoes.php
@@ -11,6 +11,16 @@ $dados = [];
 if(file_exists(PATH_HOME . "_config/config.json"))
     $dados = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), true);
 
+if(file_exists(PATH_HOME . "public/integracoes")){
+    foreach (\Helpers\Helper::listFolder(PATH_HOME . "public/integracoes") as $c) {
+        $file = json_decode(file_get_contents(PATH_HOME . "public/integracoes/{$c}"), !0);
+        foreach ($file['constantes'] as $nome => $column)
+            $file['constantes'][$nome]['value'] = $dados[$file['constantes'][$nome]['column']] ?? "";
+
+        $integ['integration'][] = $file;
+    }
+}
+
 foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR) as $item) {
     if(file_exists(PATH_HOME . VENDOR . $item . "/public/integracoes")){
         foreach (\Helpers\Helper::listFolder(PATH_HOME . VENDOR . $item . "/public/integracoes") as $c) {
