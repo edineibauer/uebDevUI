ba2acd30bc4d47103805f3b8f6d83531d2d6be45
Edinei J. Bauer <galera.org@gmail.com>
2020-06-22 17:00:04 -0300

    update session

--- public/set/panel.php
+++ public/set/panel.php
@@ -6,7 +6,7 @@ $menus = [];
 
 if ($_SESSION['userlogin']['id'] === "1")
     $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/UIDev/inc/menuMaster.json"), !0);
-elseif ($_COOKIE['setor'] === "admin")
+elseif ($_SESSION['userlogin']['setor'] === "admin")
     $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/UIDev/inc/menu.json"), !0);
 
 foreach ($menus as $menu)
--- public/view/UIDev/inc/menu.php
+++ public/view/UIDev/inc/menu.php
@@ -5,7 +5,7 @@ $tpl = new \Helpers\Template("dev-ui");
 $menus = [];
 if ($_SESSION['userlogin']['id'] === "1")
     $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/UIDev/inc/menuMaster.json"), !0);
-elseif ($_COOKIE['setor'] === "admin")
+elseif ($_SESSION['userlogin']['setor'] === "admin")
     $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/UIDev/inc/menu.json"), !0);
 
 foreach ($menus as $menu)
