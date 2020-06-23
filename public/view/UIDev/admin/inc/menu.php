fc7d65da9cda119eb3586562d7adf30e5157155f
Edinei J. Bauer <galera.org@gmail.com>
2020-06-23 08:40:54 -0300

    move views to admin setor

--- public/view/UIDev/admin/inc/menu.php
+++ public/view/UIDev/admin/inc/menu.php
@@ -1,15 +1,12 @@
 <?php
 
 $tpl = new \Helpers\Template("dev-ui");
-$data = ["response" => 1, "data" => "<div style='float: left; width: 100%; padding: 40px 5px'>", "error" => ""];
-$menus = [];
 
+$menus = [];
 if ($_SESSION['userlogin']['id'] === "1")
     $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/UIDev/admin/inc/menuMaster.json"), !0);
 elseif ($_SESSION['userlogin']['setor'] === "admin")
     $menus = json_decode(file_get_contents(PATH_HOME . VENDOR . "dev-ui/public/view/UIDev/admin/inc/menu.json"), !0);
 
 foreach ($menus as $menu)
-    $data['data'] .= $tpl->getShow("menu-card", $menu);
-
-$data['data'] .= "</div>";
+    $tpl->show("menu-li", $menu);
