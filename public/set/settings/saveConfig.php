de02b8a49b5312696221786f9a4ebdd8ebf1a348
Edinei J. Bauer <galera.org@gmail.com>
2020-07-02 11:02:53 -0300

    allow save striptags values integration

--- public/set/settings/saveConfig.php
+++ public/set/settings/saveConfig.php
@@ -1,6 +1,6 @@
 <?php
 $field = strtolower(trim(strip_tags(filter_input(INPUT_POST, "field", FILTER_DEFAULT))));
-$value = trim(strip_tags(filter_input(INPUT_POST, "value", FILTER_DEFAULT)));
+$value = trim(filter_input(INPUT_POST, "value", FILTER_DEFAULT));
 
 //Abre json config and add/update field
 $config = json_decode(file_get_contents(PATH_HOME . "_config/config.json"), !0);
