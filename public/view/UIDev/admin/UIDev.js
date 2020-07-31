d5f7b65ebfa71ac989120f690872ff085aa8a877
Edinei J. Bauer <galera.org@gmail.com>
2020-07-31 08:31:38 -0300

    remove sidebar user perfil

--- public/view/UIDev/admin/UIDev.js
+++ public/view/UIDev/admin/UIDev.js
@@ -10,39 +10,7 @@ function mainLoading() {
     closeSidebar();
 }
 
-function devSidebarInfo() {
-    if (USER.imagem === "" || USER.imagem === "null" || isEmpty(USER.imagem)) {
-        document.querySelector("#dev-sidebar-imagem").innerHTML = "<i class='material-icons font-jumbo'>people</i>"
-    } else {
-        document.querySelector("#dev-sidebar-imagem").innerHTML = "<img src='" + decodeURIComponent(JSON.parse(USER.imagem)[0]['urls'][100]) + "' height='60' width='60'>"
-    }
-    document.querySelector("#dev-sidebar-nome").innerHTML = USER.nome;
-
-    $("#core-sidebar-edit")
-        .removeClass("hide")
-        .off("click")
-        .on("click", function () {
-            if (document.querySelector(".btn-edit-perfil") !== null) {
-                document.querySelector(".btn-edit-perfil").click();
-            } else {
-                mainLoading();
-                app.loadView(HOME + "dashboard");
-                toast("carregando perfil...", 1300, "toast-success");
-                let ee = setInterval(function () {
-                    if (document.querySelector(".btn-edit-perfil") !== null) {
-                        setTimeout(function () {
-                            document.querySelector(".btn-edit-perfil").click();
-
-                        }, 1000);
-                        clearInterval(ee);
-                    }
-                }, 100);
-            }
-        });
-}
-
 $(function () {
-    devSidebarInfo();
     $("body").off("click", ".menu-li:not(.not-menu-li)").on("click", ".menu-li:not(.not-menu-li)", function () {
         let action = $(this).attr("data-action");
         mainLoading();
--- public/view/UIDev/admin/UIDev.php
+++ public/view/UIDev/admin/UIDev.php
@@ -1,21 +1,6 @@
 <nav class="sidebar z-depth-2 collapse color-gray-light no-select animate-left dev-nav space-header"
      id="mySidebar">
-    <div class="container row padding-4" style="background-color: #e9e9e9">
-        <div id="dev-sidebar-imagem" class="col" style="height: 60px; width: 60px"></div>
-        <div class="rest padding-left padding-bottom">
-            <strong class="col padding-top no-select" id="dev-sidebar-nome"></strong>
-
-            <div class="col">
-                <span class="btn-edit-perfil left pointer menu-li padding-small color-gray-light opacity hover-opacity-off hover-shadow radius"
-                      data-action="form" data-entity="usuarios"
-                      data-atributo="<?= $_SESSION['userlogin']['id'] ?>">
-                    <i class="material-icons left font-large">edit</i>
-                </span>
-            </div>
-        </div>
-    </div>
-    <hr style="margin:0 0 10px 0;border-top: solid 1px #ddd;">
-    <div class="bar-block">
+    <div class="bar-block" style="padding-top: 20px">
         <?php
         require_once 'inc/menu.php';
         ?>
