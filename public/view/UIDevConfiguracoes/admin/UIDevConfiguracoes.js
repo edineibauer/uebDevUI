2246d5077d411975f2c84e26c09b1771d65ee34e
Edinei J. Bauer <galera.org@gmail.com>
2020-08-02 11:42:18 -0300

    not get password

--- public/view/UIDevConfiguracoes/admin/UIDevConfiguracoes.js
+++ public/view/UIDevConfiguracoes/admin/UIDevConfiguracoes.js
@@ -6,22 +6,16 @@ $(function () {
 
     $("#app").off("click", "#clear-cache").on("click", "#clear-cache", function () {
         if(navigator.onLine) {
-            if(senha = prompt("Senha:")) {
-                toast("Atualizando Sistema...", 1000000000);
-                AJAX.post("updateSystem", {pass: senha}).then(g => {
-                    if(g) {
-                        if(typeof sseSource !== "undefined" && navigator.onLine && typeof (EventSource) !== "undefined")
-                            sseSource.close();
+            toast("Atualizando Sistema...", 1000000000);
+            AJAX.post("updateSystem", {pass: senha}).then(g => {
+                    if(typeof sseSource !== "undefined" && navigator.onLine && typeof (EventSource) !== "undefined")
+                        sseSource.close();
 
-                        localStorage.removeItem('update');
-                        checkUpdate().then(() => {
-                            location.href = HOME + "dashboard";
-                        })
-                    } else {
-                        toast("Senha inválida", 2000, "toast-warning");
-                    }
-                });
-            }
+                    localStorage.removeItem('update');
+                    checkUpdate().then(() => {
+                        location.href = HOME + "dashboard";
+                    })
+            });
         } else {
             toast("Sem Conexão", 2500, "toast-warning");
         }
